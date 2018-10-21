28e029a53b2a1edc86d8bb47c4d53a53bd6877db
Joshua King <king.joshua.s@gmail.com>
2018-10-21 18:52:21 +0800

    embed svg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -12,7 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostBinding, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  group,
+  stagger,
+  query
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      transition(':enter', group([
+        query('#svg8', animate('1000ms 250ms ease',
+        style({
+          transform: 'rotate(15deg)'
+        }))),
+      ]))
+    ])
+  ],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit {
+  private wheel: ElementRef;
 
+  @HostBinding('@myAwesomeAnimation')
+
+  public animateProfile = true;
+
+  constructor(private elRef: ElementRef) {
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.wheel = this.elRef.nativeElement.querySelector('#svg8');
+  }
 }
